8a5e34988d6ce3c352346f6d72c97ffcedfbb6a6
Success Obasi <obasisuccess192@gmail.com>
2024-04-17 14:16:53 +0100

    push-u

--- src/app/introduction/page.tsx
+++ src/app/introduction/page.tsx
@@ -1,66 +1,19 @@
 import Image from 'next/image';
+import { Barlow, Freckle } from "../../fonts/fonts";
 
 export default function Page() {
     return (
         <div className='flex justify-center flex-col items-center'>
-            <div className='flex gap-4'> 
-               <div className="bg-white w-[55px] h-[55px]  icon rounded-full flex items-center justify-center border-4 border-rock-1">
-                    <Image
-                        className="p-2"
-                        src="/images/icon-rock.svg"
-                        alt="Rock"
-                        width={50}
-                        height={50}
-                        priority
-                    />
-                </div>
-                
-                <div className="bg-white w-[55px] h-[55px]  icon rounded-full flex items-center justify-center border-4 border-paper-1">
-                    <Image
-                        className="p-2"
-                        src="/images/icon-paper.svg"
-                        alt="Paper"
-                        width={50}
-                        height={50}
-                        priority
-                    />
-                </div>
-                
-                <div className="bg-white w-[55px] h-[55px]  icon rounded-full flex items-center justify-center border-4 border-scissors-1">
-                    <Image
-                        className="p-2"
-                        src="/images/icon-scissors.svg"
-                        alt="Scissors"
-                        width={50}
-                        height={50}
-                        priority
-                    />
-                </div>
-                
-                <div className="bg-white w-[55px] h-[55px]  icon rounded-full flex items-center justify-center border-4 border-lizard-1">
-                    <Image
-                        className="p-2"
-                        src="/images/icon-lizard.svg"
-                        alt="Lizard"
-                        width={50}
-                        height={50}
-                        priority
-                    />
-                </div>
 
-                <div className="bg-white w-[55px] h-[55px]  icon rounded-full flex items-center justify-center border-4 border-spock-1">
-                <Image
-                    className="p-2"
-                    src="/images/icon-spock.svg"
-                    alt="Spock"
-                    width={50}
-                    height={50}
-                    priority
-                />
-                </div>
-            </div>
+            <h1 className={`${Freckle.className} text-center text-xl game-title flex justify-center items-center space-x-2 mt-8`}>
+                <span className='title text-rock-1'>Rock</span>
+                <span className='title text-paper-1'>Paper</span>
+                <span className='title text-scissors-1'>Scissors</span>
+                <span className='title text-lizard-1'>Lizard</span>
+                <span className='title text-spock-1'>Spock</span>
+            </h1>
             <div>
-                <p className = {` text-animation text-center text-white mt-4 p-4 leading-7`}>In a world where choices spark, <br/>
+                <p className = {` text-animation text-center text-white mt-4 p-4 leading-7 text-sm`}>In a world where choices spark, <br/>
                 There&apos;s <span className="text-rock-1">Rock </span>
                 <span className="text-paper-1">Paper </span> 
                 <span className="text-scissors-1">Scissors </span> 
