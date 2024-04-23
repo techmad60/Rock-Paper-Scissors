96f3d279bb982cbb9a3bc25d18a7f21efd5fd5f2
Success Obasi <obasisuccess192@gmail.com>
2024-04-23 10:28:41 +0100

    cold

--- src/app/level-one/page.tsx
+++ src/app/level-one/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image"
 export default function LevelOne() {
 
     return (
-      <div className={`${Freckle.className}flex justify-center 
-      flex-col `}>
-         <p className="text-center text-white"> Level 1 </p>
+      <div className={`${Freckle.className} flex justify-center 
+      flex-col`}>
+         <p className={`${Freckle.className} text-center text-white`}> Level 1 </p>
          <div className="flex flex-col border-[3px] border-header-outline rounded-md p-1">
             <div className={` ${Barlow.className} flex justify-between items-center  m-1`}>
                <div className={` ${Barlow.className} text-white font-semibold text-xl leading-4 pl-4`}> 
@@ -21,36 +21,37 @@ export default function LevelOne() {
             </div>
          </div>
 
-         <div className="bg-triangle bg-no-repeat flex flex-col justify-center items-center  top-48 relative w-full h-full"> 
-           <div className="bg-white w-[150px] h-[150px] icon rounded-full flex items-center justify-center border-[18px] border-rock-1 absolute">
+         <div className="bg-triangle bg-no-repeat flex flex-col justify-center items-center self-center justify-self-center relative w-[250px] h-[250px] top-40 bg-contain"> 
+           <div className="bg-white w-[120px] h-[120px] icon rounded-full flex items-center justify-center border-[18px] border-rock-2 absolute top-[-70px] right-[160px]">
                 <Image
-                    className="p-2"
+                    className="p-4"
                     src="/images/icon-rock.svg"
                     alt="Rock"
-                    width={75}
-                    height={75}
+                    width={70}
+                    height={70}
                     priority
                 />
             </div>
             
-            <div className="bg-white w-[150px] h-[150px]  icon rounded-full flex items-center justify-center border-[18px] border-paper-1 absolute ">
+            <div className="bg-white w-[120px] h-[120px]  icon rounded-full flex items-center justify-center border-[18px] border-paper-2 absolute top-[-70px] left-[160px]">
                 <Image
-                    className="p-2"
+                    className="p-4"
                     src="/images/icon-paper.svg"
                     alt="Paper"
-                    width={75}
-                    height={75}
+                    width={70}
+                    height={70}
                     priority
                 />
             </div>
             
-            <div className="bg-white w-[150px] h-[150px]  icon rounded-full flex items-center justify-center border-[18px] border-scissors-1 absolute">
+            <div className="bg-white w-[120px] h-[120px]  icon rounded-full flex items-center justify-center border-[18px] border-scissors-2 absolute 
+            top-[100px]">
                 <Image
-                    className="p-2"
+                    className="p-4"
                     src="/images/icon-scissors.svg"
                     alt="Scissors"
-                    width={75}
-                    height={75}
+                    width={70}
+                    height={70}
                     priority
                 />
             </div>
