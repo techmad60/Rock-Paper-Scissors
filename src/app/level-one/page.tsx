5ef1cade26c773fbb63b705fa52910be595b51f4
Success Obasi <obasisuccess192@gmail.com>
2024-04-28 20:23:57 +0100

    omo

--- src/app/level-one/page.tsx
+++ src/app/level-one/page.tsx
@@ -154,7 +154,7 @@ export default function LevelOne() {
                     />
                 </div>
             </div>
-            <div className={`${Barlow.className} flex justify-center items-center mt-44 rounded-lg border-2 w-40 border-white self-center justify-self-center tracking-wider text-xl hover:bg-white hover:border-dark text-white transition ease-in-out delay-150 hover:text-dark-text button`}>
+            <div className={`${Barlow.className} flex justify-center items-center mt-44 rounded-lg border-2 w-40 border-white self-center justify-self-center tracking-wider text-xl hover:bg-white hover:border-dark text-white hover:text-dark-text button`}>
                 <Link href ={"/rules"} className="p-2"> RULES </Link>
             </div>
         </div>
